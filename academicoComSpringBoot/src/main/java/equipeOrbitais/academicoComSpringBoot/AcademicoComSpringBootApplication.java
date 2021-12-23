cf346109eada9f6b3cb5258a042100e279157fcf
ithaelly <ithaellymykarlla@gmail.com>
2021-12-23 09:46:39 -0300

    Tirado comando desnecessários

--- academicoComSpringBoot/src/main/java/equipeOrbitais/academicoComSpringBoot/AcademicoComSpringBootApplication.java
+++ academicoComSpringBoot/src/main/java/equipeOrbitais/academicoComSpringBoot/AcademicoComSpringBootApplication.java
@@ -2,6 +2,7 @@ package equipeOrbitais.academicoComSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener;
 
 @SpringBootApplication
 public class AcademicoComSpringBootApplication {
